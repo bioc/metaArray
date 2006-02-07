a51a2eb04d9198ece7316d70e3dc58f2e59629ec
unknown <hwchoi@umich.edu>
2006-02-07 19:55:31 +0000

    fit.em updated and Rnw/Tex
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metaArray@16131 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fit.em.R
+++ R/fit.em.R
@@ -1,16 +1,13 @@
 "fit.em" <-
-function(x, cl, threshold=0.00001){ 
+function(x, cl=rep(0,length(x)), threshold=0.0001){ 
   conv <- T
   dec <- F
   abnormal <- F
   x <- as.numeric(x)
-  nx <- length(x)
-  ind1 <- (1:nx)[!is.na(x) & cl==1]
-  ind2 <- (1:nx)[!is.na(x) & cl==0]
-  x2 <- x[ind1]
-  tt <- length(x2)
-  z <- rep(0,length(x2))
-  id <- find.init(x2)
+  tt <- length(x)
+  z <- rep(0,length(x))
+  if(all(cl==0)) id <- find.init(x)
+  else id <- cl    
   log.lik <- 1000
   lik.rec <- NULL
   num.iter <- 0
@@ -19,56 +16,77 @@ function(x, cl, threshold=0.00001){
     abnormal <- T
     lik.rec <- 0
   }
-  z[id == 1] <- 1
+  if(all(cl==0)) z[id == 1] <- 1
+  else z <- cl/2
+
   a <- min(x,na.rm=T); b <- max(x,na.rm=T)
-  a <- a - (b-a)*.0001; b <- b + (b-a)*.0001  
+  #a <- a - (b-a)*.001; b <- b + (b-a)*.001  
   err <- err.old <- 1
-  pi <- sum(z) / tt 
-  mu <- sum((1-z)*x2) / sum(1-z)
-  sigmasq <- sum((1-z)*(x2-mu)^2) / sum(1-z)
+
+  if(all(cl==0)) pi <- sum(z) / tt
+  else pi <- sum(z[cl==1]) / sum(cl)
+
+  mu <- sum((1-z)*x) / sum(1-z)
+  sigmasq <- sum((1-z)*(x-mu)^2) / sum(1-z)
+
   while(err > threshold & conv == T) {
     num.iter <- num.iter + 1
     log.lik.old <- log.lik
     err.old <- err
     z.old <- z
    ## E Step
-    est.u <- dunif(x2,a,b)
+    est.u <- dunif(x,a,b)
     est.u.p <- pi * est.u
-    est.n <- dnorm(x2,mu,sqrt(sigmasq))
+    est.n <- dnorm(x,mu,sqrt(sigmasq))
     est.n.p <- (1-pi) * est.n
-    z <- est.u.p / (est.n.p + est.u.p)
+    if(all(cl==0)) z <- est.u.p / (est.n.p + est.u.p)
+    else {
+      z <- est.u.p / (est.n.p + est.u.p) #[cl==1]
+      z[cl==0] <- 0
+    }
+
     if(any(is.na(z))) conv <- F
    ## M Step
-    pi <- sum(z) / tt 
-    sigmasq <- sum((1-z)*((x2-mu)^2)) / sum(1-z)
-    mu <- sum((1-z)*x2) / sum(1-z)
-    sgn.z <- ifelse(x2 < mu, -1, 1)  
+    if(all(cl==0)) pi <- sum(z) / tt
+    else pi <- sum(z[cl==1]) / sum(cl)
+    mu <- sum((1-z)*x) / sum(1-z)
+    sigmasq <- sum((1-z)*((x-mu)^2)) / sum(1-z)
+
+    sgn.z <- ifelse(x < mu, -1, 1)  
     est.u[est.u==0] <- 10e-2 
     est.n[est.n==0] <- 10e-2
-    log.lik <- sum(log(est.u.p + est.n.p))
+    
+    if(all(cl==0)) log.lik <- sum(log(est.u.p + est.n.p))
+    else log.lik <- sum(log(est.n[cl==0])) + sum(log(est.u.p[cl==1] + est.n.p[cl==1]))
+    #cat(log.lik, "\n")
     err <- abs(log.lik.old - log.lik)
     if(num.iter != 1) lik.rec[num.iter-1] <- log.lik
     dec <- ifelse(log.lik.old > log.lik, T, F)
     if(any(pi==0 | pi==1)) err <- 0
   }
-  x3 <- x[ind2]
-  tt <- length(x3)
-  z0 <- rep(0,tt)
-  est.u.p <- pi * dunif(x3,a,b)
-  est.n.p <- (1-pi) * dnorm(x3,mu,sqrt(sigmasq))
+
+  if(all(cl==0)) {
+    est.u.p <- pi * dunif(x,a,b) 
+    est.n.p <- (1-pi) * dnorm(x,mu,sqrt(sigmasq))
+  }
+  else {
+    pi2 <- pi * sum(cl) / tt
+    est.u.p <- pi2 * dunif(x,a,b)
+    est.n.p <- (1 - pi2) * dnorm(x,mu,sqrt(sigmasq))
+  }
   z0 <- est.u.p / (est.n.p + est.u.p)
-  sgn.z0 <- ifelse(x3 < mu, -1, 1) 
+  sgn.z0 <- ifelse(x < mu, -1, 1) 
   loc <- (max(lik.rec) != lik.rec[length(lik.rec)])
   if(!conv | abnormal) {
-    expr <- rep(NA,nx)
+    expr <- rep(NA,tt)
     mu <- sigmasq <- pi <- NA
     loc <- F
     lik.rec <- 0
   }
   else {
-    expr <- rep(NA, nx)
-    expr[ind1] <- (z - min(z)) * sgn.z
-    expr[ind2] <- (z0 - min(z)) * sgn.z0  
+    expr <- rep(NA, tt)
+    expr <- (z0 - min(z0)) * sgn.z0
+
   }
   return(list(expr=expr, a=a, b=b, sigmasq=sigmasq, mu=mu, pi=pi, conv=conv, 
 dec=dec, loc=loc, lik.rec=lik.rec, abnormal=abnormal))
