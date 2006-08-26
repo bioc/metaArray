c404f402ae87e7964f8e5dc9547727065c3a71c7
unknown <hwchoi@umich.edu>
2006-08-26 00:09:28 +0000

    POE EM mixture proportion underflow prevented
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_8/madman/Rpacks/metaArray@19479 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fit.em.R
+++ R/fit.em.R
@@ -1,5 +1,5 @@
 "fit.em" <-
-function(x, cl=NULL, threshold=0.0001){ 
+function(x, cl=NULL, threshold=1e-10){ 
   conv <- TRUE
   dec <- FALSE
   abnormal <- FALSE
@@ -18,14 +18,14 @@ function(x, cl=NULL, threshold=0.0001){
     lik.rec <- 0
   }
   if(all(cl==0)) z[id == 1] <- 1
-  else z <- cl/2
+  else z <- rep(0.5,tt)
 
-  a <- min(x,na.rm=TRUE)-1e-14; b <- max(x,na.rm=TRUE)+1e-14
+  a <- min(x,na.rm=TRUE); b <- max(x,na.rm=TRUE)
   #a <- a - (b-a)*.001; b <- b + (b-a)*.001  
   err <- err.old <- 1
 
   if(all(cl==0)) pi <- sum(z) / tt
-  else pi <- sum(z[cl==1]) / tt #sum(cl)
+  else pi <- sum(cl) / tt
 
   mu <- sum((1-z)*x) / sum(1-z)
   sigmasq <- sum((1-z)*(x-mu)^2) / sum(1-z)
@@ -40,35 +40,40 @@ function(x, cl=NULL, threshold=0.0001){
     est.u.p <- pi * est.u
     est.n <- dnorm(x,mu,sqrt(sigmasq))
     est.n.p <- (1-pi) * est.n
-    if(all(cl==0)) z <- est.u.p / (est.n.p + est.u.p)
-    else {
-      z <- est.u.p / (est.n.p + est.u.p) #[cl==1]
-      z[cl==0] <- 0
-    }
+    z <- est.u.p / (est.n.p + est.u.p)
 
     if(any(is.na(z))) conv <- FALSE
    ## M Step
-    if(all(cl==0)) pi <- sum(z) / tt
-    else pi <- sum(z[cl==1]) / tt #sum(cl)
+    if(any(cl==1)) z[cl==0] <- 0
+    pi <- sum(z) / tt
+    if(any(cl==1)) pi <- max(pi, 1 / tt)
+    #if(any(cl==1)) pi <- sum(z) / sum(cl)
     mu <- sum((1-z)*x) / sum(1-z)
     sigmasq <- sum((1-z)*((x-mu)^2)) / sum(1-z)
 
     sgn.z <- ifelse(x < mu, -1, 1)  
-    est.u[est.u==0] <- 10e-2 
-    est.n[est.n==0] <- 10e-2
+    #est.u[est.u==0] <- 1e-15 
+    #est.n[est.n==0] <- 1e-15
+
+    est.u <- dunif(x,a,b)
+    est.u.p <- pi * est.u
+    est.n <- dnorm(x,mu,sqrt(sigmasq))
+    est.n.p <- (1-pi) * est.n
     
-    if(all(cl==0)) log.lik <- sum(log(est.u.p + est.n.p))
-    else log.lik <- sum(log(est.n[cl==0])) + sum(log(est.u.p[cl==1] + est.n.p[cl==1]))
-    #cat(log.lik, "\n")
+    log.lik <- sum(log(est.u.p + est.n.p))
     err <- abs(log.lik.old - log.lik)
     if(num.iter != 1) lik.rec[num.iter-1] <- log.lik
     dec <- ifelse(log.lik.old > log.lik, TRUE, FALSE)
     if(any(pi==0 | pi==1)) err <- 0
   }
 
+  if(any(cl==1)) pi <- max(1/tt,pi)
   est.u.p <- pi * dunif(x,a,b) 
   est.n.p <- (1-pi) * dnorm(x,mu,sqrt(sigmasq))
+  est.u.mu <- pi * dunif(mu,a,b)
+  est.n.mu <- (1-pi) * dnorm(mu,mu,sqrt(sigmasq))
   z0 <- est.u.p / (est.n.p + est.u.p)
+  zmu <- est.u.mu / (est.n.mu + est.u.mu)
   sgn.z0 <- ifelse(x < mu, -1, 1) 
   loc <- (max(lik.rec) != lik.rec[length(lik.rec)])
   if(!conv | abnormal) {
@@ -79,8 +84,7 @@ function(x, cl=NULL, threshold=0.0001){
   }
   else {
     expr <- rep(NA, tt)
-    expr <- (z0 - min(z0)) * sgn.z0
-
+    expr <- (z0-zmu) * sgn.z0
   }
   return(list(expr=expr, a=a, b=b, sigmasq=sigmasq, mu=mu, pi=pi, conv=conv, dec=dec, loc=loc, lik.rec=lik.rec, abnormal=abnormal))
 }
